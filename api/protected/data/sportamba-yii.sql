9460db651b1e9ea55a8aefc6a6357e0cd0c89199
grischuk2703 <grischuk.alexandr@gmail.com>
2014-04-08 00:37:24 +0300

    fix

--- api/protected/data/sportamba-yii.sql
+++ api/protected/data/sportamba-yii.sql
@@ -96,6 +96,9 @@ CREATE TABLE IF NOT EXISTS `sport_user_profile` (
 INSERT INTO `sport_user` (`id`, `username`, `password`, `session_data`, `created_at`, `provider`, `role`, `status`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '', '2014-03-31 00:00:00', '', 'admin', 1);
 
+INSERT INTO `sport_user_profile` (`id`, `user_id`, `profileUrl`, `photoUrl`, `displayName`, `firstName`, `lastName`, `gender`, `region`, `email`) VALUES
+(1, 1, '', '', '', 'admin', '', '', NULL, NULL);
+
 DROP TABLE IF EXISTS `sport_stadion`;
 
 CREATE TABLE IF NOT EXISTS `sport_stadion` (
